6435e59c3b5c939c93159eb44cd6230e5437983f
Irvan Arif <114166811+IrvanArif@users.noreply.github.com>
2022-10-18 10:48:39 +0700

    Create Honda.java
    Aduh buyung....

---
+++ Honda.java
@@ -0,0 +1,15 @@
+class Honda extends PabrikanMotor {
+
+   Honda() {
+        System.out.println("_______________________________");
+    }
+   @Override
+    protected void PabrikanMotor() {
+        
+        System.out.println("Honda ");
+    }
+    public static void main (String[] args) {
+        System.out.println("_______________________________");
+
+}
+}
