28433be4f6bf4ec4f59f8036004a360f84133b64
Irvan Arif <114166811+IrvanArif@users.noreply.github.com>
2022-10-18 10:50:13 +0700

    Create Yamaha.java
    aduh emak

---
+++ Yamaha.java
@@ -0,0 +1,15 @@
+kelas Yamaha memperluas PabrikanMotor {
+
+   Yamaha() {
+        System.out.println("________________________________");
+    }
+   @Mengesampingkan
+    dilindungi void PabrikanMotor() {
+        
+        System.out.println("Yamaha");
+    }
+    public static void main (String[] args) {
+        System.out.println("________________________________");
+
+}
+}
